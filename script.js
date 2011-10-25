03489902c53b06e50edfeedf3aeb52b93960684b
Dan DeFelippi <dan@driverdan.com>
2011-10-24 18:33:02 -0700

    Add style.css

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-(function() {
-  var s = document.createElement("script");
-  s.setAttribute("src", "http://code.jquery.com/jquery.js");
-  document.body.appendChild(s);
+(function(doc, undef) {
+  var baseUrl = "https://raw.github.com/driverdan/Hacker-News-Enhancement-Suite/master/"
+    , el = doc.createElement("script");
+  el.setAttribute("src", "http://code.jquery.com/jquery.js");
+  doc.body.appendChild(el);
 
-  s.addEventListener("load", function() {
-    var $ = jQuery.noConflict()
+  el.addEventListener("load", function() {
+    var $ = jQuery.noConflict(true)
       , $main = $("table tr:eq(4) td:eq(0)")
       , $content;
-
-    // Add CSS rules
-    $("<style>.selected {background:#ffec8b} a:visited{background:#ffe4cf}</style>").appendTo("head");
 
     // Setup layout and append content div
     $("table", $main)
     .css({width:"30%",float:"left"})
-    .parent().append('<div id="content" style="margin:0 10px">');
+    .parent().append('<div id="content">');
 
     $content = $("#content");
 
@@ -38,4 +36,10 @@
       return false;
     });
   });
-})();
+
+  // Add CSS
+  el = doc.createElement("link");
+  el.rel = "stylesheet";
+  el.href = baseUrl + "style.css";
+  doc.head.appendChild(el);
+})(document);
